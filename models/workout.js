ac0ca7dba27b429b32a69bfe144d9b532677bb4b
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-04-09 10:43:41 -0500

    Inital Models Created

---
+++ models/workout.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String, 
+            trim: true,
+            required: "Please enter an exercise type"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Please enter an exercise name"
+        },
+        duration: {
+            type: Number,
+            trim: true,
+            required: "Please enter a duration in minutes"
+        },
+        weight: {
+            type: Number,
+            trim: true,
+        },
+       reps: {
+            type: Number,
+            trim: true,
+        },
+        sets: {
+            type: Number,
+            trim: true,
+        },
+        distance: {
+            type: Number,
+            trim: true,
+        },
+    }]
+}, {
+    toJSON: {
+        virtuals: true
+    }
+});
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
