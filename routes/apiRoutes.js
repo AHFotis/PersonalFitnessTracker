92ef75ea9e2d0361c300e09f8221c73a5fbec6e9
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-04-11 13:24:35 -0500

    Range route returning -- needs some debugging still

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const Workout = require("../models/workoutModel.js");
 
 //retrieves all workouts
 router.get("/api/workouts", (req, res) => {
-    Workout.find()
+    Workout.find({})
     .then(workout => {
       res.json(workout);
     })
@@ -38,7 +38,20 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
 
 //returns data to states page. need to study aggregates for this.
 router.get("/api/workouts/range", (req, res) => {
-
+Workout.aggregate([
+  {
+      $addFields: {
+          totalWeight: { $sum: "$exercises.weight" },
+          totalDuration: { $sum: "$exercises.duration" }
+      }
+  },
+]).sort({ day: - 1 }).limit(7).sort( { day: 1 })
+.then(workout => {
+    res.json(workout);
+  })
+  .catch(err => {
+    res.json(err);
+  });
 });
 
 module.exports = router
